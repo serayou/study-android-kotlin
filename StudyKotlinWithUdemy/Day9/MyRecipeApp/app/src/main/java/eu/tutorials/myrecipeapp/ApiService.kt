86f1d038253896d7e568c698e431edfadb51f5ce
serayou <gyh02035@gmail.com>
2024-05-29 01:09:02 +0900

    Day9 - MyRecipeApp 프로젝트 추가

---
+++ StudyKotlinWithUdemy/Day9/MyRecipeApp/app/src/main/java/eu/tutorials/myrecipeapp/ApiService.kt
@@ -0,0 +1,18 @@
+package eu.tutorials.myrecipeapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+    val recipeService = retrofit.create(ApiService::class.java)
+
+interface ApiService{
+    @GET("categories.php") //URL의 마지막 부분(어떤 파일에서 데이터를 받아오는지)
+    suspend fun getCategories():CategoriesResponse
+
+}
