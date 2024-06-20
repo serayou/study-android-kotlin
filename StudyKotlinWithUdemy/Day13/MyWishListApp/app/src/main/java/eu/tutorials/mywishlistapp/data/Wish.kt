93d4b88864ba2284c202c220b8387739c0e625b9
serayou <gyh02035@gmail.com>
2024-06-20 13:52:35 +0900

    Day14 - room 데이터 저장을 위한 테이블 생성

--- StudyKotlinWithUdemy/Day13/MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/data/Wish.kt
+++ StudyKotlinWithUdemy/Day13/MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/data/Wish.kt
@@ -1,8 +1,16 @@
 package eu.tutorials.mywishlistapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+    @ColumnInfo("wish-title")
     val title: String="",
+    @ColumnInfo("wish-description")
     val description:String=""
 )
 
